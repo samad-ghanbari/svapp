6f25d301c536e1cad787d10e375833b01fc9d082
Samad Ghanbari <ghanbari.samad@gmail.com>
2025-08-25 00:06:17 +0330

    home page init

---
+++ src/lib/components/Toolbar.svelte
@@ -0,0 +1,13 @@
+<div class="top-0 flex h-[50px] w-full justify-between">
+	<button class="h-[50px] w-[50px] cursor-pointer border-0 bg-transparent"
+		><img src="/assets/images/help.png" alt="help iSchool" /></button
+	>
+
+	<button class="h-[50px] w-[50px] cursor-pointer border-0 bg-transparent"
+		><img src="/assets/images/home2.png" alt="home iSchool" /></button
+	>
+
+	<button class="h-[50px] w-[50px] cursor-pointer border-0 bg-transparent"
+		><img src="/assets/images/arrow-right.png" alt="back" /></button
+	>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
-    // You can add any logic here if needed
-    import {CONFIGS} from '$lib/constants'
+	// You can add any logic here if needed
+	import { CONFIGS } from '$lib/constants';
+	import Toolbar from '$lib/components/Toolbar.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<div class="fixed inset-0 flex items-center justify-center bg-gradient-to-br from-blue-10 to-blue-100 text-white text-2xl z-[9999]">
+<svelte:head>
+	<title>iSchool &#x1F3E0</title>
+	<!-- &#x... -->
+</svelte:head>
+
+<div class="from-blue-10 fixed inset-0 z-[9999] bg-gradient-to-br to-blue-100 text-2xl text-black">
+	<Navbar />
+	<Toolbar />
 </div>
