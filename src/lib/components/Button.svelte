00cae415b9c077bef642fb9e75799b275fc7bfed
Samad Ghanbari <Ghanbari.Samad@gmail.com>
2025-10-26 17:15:50 +0330

    start mod

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { FlowbiteSolid } from 'flowbite-svelte-icons';
+	import { Button } from 'flowbite-svelte';
+
+	const { icon, name, action } = $props<{ icon: string; name: string; action: string }>();
+
+	let state: boolean = $state(false);
+
+	function handleClick() {
+		console.log('Button clicked!');
+	}
+</script>
+
+<div class="bg-bl">
+	<Button on:click={handleClick} color="primary">Click Me</Button>
+</div>
